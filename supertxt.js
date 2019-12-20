c4b6993011306f39aa2688731e1f9cda7df13989
thisusernameisavailable <58608177+thisusernameisavailable@users.noreply.github.com>
2019-12-20 17:56:37 -0500

    Update supertxt.js

--- supertxt.js
+++ supertxt.js
@@ -1,3 +1,14 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
 (function(ext) {
     var i = 0;
     // Cleanup function when the extension is unloaded
@@ -10,21 +21,18 @@
     };
     ext.AddText = function(added, id){
         let add = document.createElement('div');
-        div.id = "added";
+        div.className = "added";
         div.innerHTML = "<p id=" + id + ">" + added + "</p>";
         i++;
-        document.body.append(div);
-    }
-    ext.FontChange = function(id, font){
-        document.getElementById(id).style.fontFamily = font;
-    }
+  document.body.append(div);
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             [' ', 'Include text: %s with id: %s', 'AddText', "Hello World!", "hello"],
             [' ', 'Change font of id: %s to: %m.fonts', 'FontChange', 1, 'Sans Serif'],
-        ],
+        ]
         menus: {
             fonts: ['Sans Serif', 'Serif', 'Comic Sans'],
         },
