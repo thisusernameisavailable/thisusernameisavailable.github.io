7ad58688439bf0446eca71f0b8b94ea0721be902
thisusernameisavailable <58608177+thisusernameisavailable@users.noreply.github.com>
2020-01-03 15:37:12 -0500

    Update supertxt.js

--- supertxt.js
+++ supertxt.js
@@ -1,13 +1,4 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
+
 
 (function(ext) {
     var i = 0;
